77934c9c9e66996b2c8613298c8ecee401d1fb9c
Sambat <doung.ratanaksambat19@kit.edu.kh>
2020-07-30 22:13:51 +0700

    updated

--- reviewDemo/urls.py
+++ reviewDemo/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from useracc import views
+from register import views as v_re
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('admin/', admin.site.urls),
-    path('useracc/', include('useracc.urls'))
+    path('useracc/', include('useracc.urls')),
+    path('register/', v_re.register, name='register')
 ]
